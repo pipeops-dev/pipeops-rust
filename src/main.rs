84afa05ad31f7b578d625a0746c12c1ecd292a5d
Alex Idowu <alexidowu25@gmail.com>
2024-05-05 19:43:20 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = env::var("PORT").unwrap_or_else(|_| String::from("8080")); // Default to 8080 if PORT not found
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+    })
+    .bind(format!("0.0.0.0:{}", port))?
+    .run()
+    .await
 }
